055300bccd23ff7ca81a9bcc5b2d05b274c56c92
zh.nurbekov <zh.nurbekov@gmail.com>
2022-08-18 21:55:56 +0600

    remove moment lib

--- src/_helpers/translate.js
+++ src/_helpers/translate.js
@@ -1,12 +1,7 @@
 import i18next from 'i18next';
 import {initReactI18next} from 'react-i18next';
-import moment from 'moment';
 
 let language = 'ru';
-if (window.localStorage.getItem('lang')) {
-	language = window.localStorage.getItem('lang');
-	moment.locale(language);
-}
 i18next.use(initReactI18next).init({lng: language}).then(() => {});
 
 export const addTranslation = (translations) => {
